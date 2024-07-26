ab503f539015abd054e4cd81c786b520aa1a2c2d
unbanlg <unbanwhatsapplegit@gmail.com>
2024-07-26 17:51:23 +0100

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -23,7 +23,7 @@ const config = {
   AUTO_REJECT_CALLS: process.env.AUTO_REJECT_CALLS !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || 'private',
-  PREFIX: process.env.PREFIX || '.',
+  PREFIX: process.env.PREFIX || '!',
   PORT: process.env.PORT || "5000", /* Where App Will Listen On */
   OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "254762016957",
